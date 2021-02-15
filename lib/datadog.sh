cf0e9fc5f6ee5dd3ec4297fa171451aa8ed81edd
Carlos Ré <carlos.re@mercedes-benz.io>
2021-02-15 18:51:00 +0000

    change dh-io-datadog dir

--- lib/datadog.sh
+++ lib/datadog.sh
@@ -2,5 +2,5 @@
 
 echo "Starting Datadog decorator (dh-io-datadog)"
 
-SCRIPTS_DIR="${DHIO_DATADOG_DIR:-/home/vcap/app/dh-io-datadog}"
+SCRIPTS_DIR=/home/vcap/app/dh-io-datadog
 python $SCRIPTS_DIR/datadog.py
