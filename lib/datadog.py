1ab20212a5673e96d191a7a221d9fcbd7bf3edd8
Carlos Ré <carlos.re@mercedes-benz.io>
2021-02-15 17:56:31 +0000

    change datadog service find to tag based

--- lib/datadog.py
+++ lib/datadog.py
@@ -2,56 +2,60 @@ import os
 import sys
 import json
 
+SERVICE_TAG = 'datadog'
+
+
 def main():
     appinfo = get_application_info()
     service = find_datadog_service()
-    if service != None:
-        inject_datadog(service, appinfo)
+    inject_datadog(service, appinfo)
 
-def detect():
-    appinfo = get_application_info()
-    service = find_datadog_service()
-    if service == None:
-        print >> sys.stderr, "Can't find dh-io-datadog service"
-        sys.exit(1)
 
-# Get Application Info
-#
-# Collect the information about the application
-#
 def get_application_info():
+    """ Collect the information about the application """
+
     appinfo = {}
     vcap_application = json.loads(os.getenv('VCAP_APPLICATION', '{}'))
     appinfo['name'] = vcap_application.get('application_name')
-    if appinfo['name'] == None:
-        print >> sys.stderr, "VCAP_APPLICATION must specify application_name"
-        sys.exit(1)
+    if not appinfo['name']:
+        abort("VCAP_APPLICATION must specify application_name")
     return appinfo
 
-# Find datadog service
-#
-def find_datadog_service():
-    vcap_services = json.loads(os.getenv('VCAP_SERVICES', '{}'))
-    for service in vcap_services:
-        service_instances = vcap_services[service]
-        for instance in service_instances:
-            if instance['name'] == 'dh-io-datadog':
-                return instance
-    return None
 
-# Injects DO env variables
+def find_datadog_service():
+    """ Find datadog service """
+    vcap_services = json.loads(os.getenv('VCAP_SERVICES', '{}'))
+    tagged_services = [s for s in vcap_services if SERVICE_TAG in s['tags']]
+    if not tagged_services:
+        abort(f"No service bind found with tag {SERVICE_TAG}")
+    datadog_service = tagged_services[0]
+    if len(tagged_services) > 1:
+        warn(f"Multiple tagged services found, using {datadog_service}")
+    return datadog_service
+
+
 # TODO:
 # - understand what to do with env variables already set
 # - undertand what to do with non DD_ variables that we will need to set
 def inject_datadog(service, appinfo):
+    """ Injects DO env variables """
     print('injecting env variables')
     env_vars = service.get('credentials', {})
     for (key, value) in env_vars.items():
         print('injecting: {}={}'.format(key, value))
-        os.environ[key]=value
+        os.environ[key] = value
     print('============= FILE LIST ========================')
     file_list = os.listdir('.')
     print(file_list)
+
+
+def warn(msg):
+    print(msg, file=sys.stderr)
+
+
+def abort(msg, rc=1):
+    print(msg, file=sys.stderr)
+    sys.exit(rc)
 
 
 if __name__ == "__main__":
--- lib/datadog.sh
+++ lib/datadog.sh
@@ -2,9 +2,5 @@
 
 echo "Starting Datadog decorator (dh-io-datadog)"
 
-#__BUILDPACK_INDEX__ gets replaced by bin/supply at cf push
-
-# this should be the right command since I believe that $DEPS_DIR gets injected when this file is called
-# but didnt prove that
-
-python $DEPS_DIR/__BUILDPACK_INDEX__/datadog.py &
+SCRIPTS_DIR="${DHIO_DATADOG_DIR:-/home/vcap/app/dh-io-datadog}"
+python $SCRIPTS_DIR/datadog.py
