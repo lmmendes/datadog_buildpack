e0f4cb239dfb7efb8208721e3c0ec5cfc377503e
Luis Mendes <luis.mendes@passworks.io>
2021-02-15 09:07:46 +0000

    fixed typo

--- lib/datadog.sh
+++ lib/datadog.sh
@@ -1,12 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 echo "Starting Datadog decorator (dh-io-datadog)"
 
 #__BUILDPACK_INDEX__ gets replaced by bin/supply at cf push
 
-# this should be the right command since I belive that $DEPS_DIR gets injected when this file is called
+# this should be the right command since I believe that $DEPS_DIR gets injected when this file is called
 # but didnt prove that
-# python $DEPS_DIR/__BUILDPACK_INDEX__/datadog.py &
 
-python ~/deps/__BUILDPACK_INDEX__/datadog.py &
-
+python $DEPS_DIR/__BUILDPACK_INDEX__/datadog.py &
